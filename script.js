605a77d93716ed960c6415c9746067cc83fada60
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-07-11 15:39:27 +0100

    now the values are being displayed but still some errors to fix

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function checkAnswer(){
-   var q1 =  document.getElementsByName("q1").keys
-   
-    console.log(q1.length)
-    document.getElementById("result").innerHTML = q1
+   var anwers =  document.getElementById('quiz');
+
+    var an1 = anwers.elements['q1'].value;
+    var an2 = anwers.elements['q2'].value;
+    document.getElementById("result").innerHTML = an1 + " ans2 " + an2;
+    console.log(an1)
    
 }
